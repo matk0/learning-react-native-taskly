9a1db33ca82bbade92eb8f12bf833bfcd69dd852
Matej <matej@occupationbliss.com>
2025-02-15 11:22:45 +0100

    Catch up with the course.

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { ShoppingListItem } from '../components/ShoppingListItem';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { ShoppingListItem } from "../components/ShoppingListItem";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container} testID="app-root">
-      <View style={[StyleSheet.absoluteFill, { backgroundColor: 'white' }]} />
+      <View style={[StyleSheet.absoluteFill, { backgroundColor: "white" }]} />
+      <Link
+        href="/counter"
+        style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}
+      >
+        Counter
+      </Link>
       <ShoppingListItem name="The Elysian Grand" />
       <ShoppingListItem name="Celestia Royal" />
       <ShoppingListItem name="Véronique Manor" />
@@ -16,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
 });
