d4f9a59b10655758a81fd3daf9b397a028fa81fa
Matej <matej@occupationbliss.com>
2025-02-17 07:53:41 +0100

    Add ScrollView.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier", "react-native"],
+  extends: ["expo"],
+  plugins: ["react-native"],
   ignorePatterns: ["/dist/*"],
   rules: {
-    "prettier/prettier": "error",
     "react-native/no-unused-styles": "error",
   },
 };
--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,81 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, Text, FlatList, View } from "react-native";
 import { ShoppingListItem } from "../components/ShoppingListItem";
+import { theme } from "../theme";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Milk" },
+  { id: "3", name: "Bread" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] =
+    useState<ShoppingListItemType[]>(initialList);
+  const [value, setValue] = useState();
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        { id: new Date().toTimeString(), name: value },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+
   return (
-    <View style={styles.container} testID="app-root">
-      <View style={[StyleSheet.absoluteFill, { backgroundColor: "white" }]} />
-      <ShoppingListItem name="The Elysian Grand" />
-      <ShoppingListItem name="Celestia Royal" />
-      <ShoppingListItem name="Véronique Manor" />
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      ListEmptyComponent={() => (
+        <View style={styles.listEmptyContainer}>
+          <Text>No items in the list</Text>
+        </View>
+      )}
+      ListHeaderComponent={
+        <TextInput
+          value={value}
+          onChangeText={setValue}
+          onSubmitEditing={handleSubmit}
+          style={styles.input}
+          placeholder="Enter item name"
+          returnKeyType="done"
+        />
+      }
+      data={shoppingList}
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      stickyHeaderIndices={[0]}
+      renderItem={({ item }) => <ShoppingListItem name={item.name} />}
+    />
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    padding: 12,
+  },
+  contentContainer: {
+    paddingBottom: 24,
+  },
+  listEmptyContainer: {
     justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 18,
+  },
+  input: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
   },
 });
