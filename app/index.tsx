6fd4b166d610376bcf0da72ba03c0a3c95df2cc8
Matej <matej@occupationbliss.com>
2025-02-20 08:41:56 +0100

    Add Haptic Feedback.

--- app/index.tsx
+++ app/index.tsx
@@ -10,6 +10,8 @@ import { ShoppingListItem } from '../components/ShoppingListItem';
 import { theme } from '../theme';
 import { useState, useEffect } from 'react';
 import { getFromStorage, saveToStorage } from '../utils/storage';
+import * as Haptics from 'expo-haptics';
+
 const storageKey = 'shopping-list';
 
 type ShoppingListItemType = {
@@ -55,12 +57,18 @@ export default function App() {
     const newShoppingList = shoppingList.filter((item) => item.id !== id);
     saveToStorage(storageKey, newShoppingList);
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
     setShoppingList(newShoppingList);
   };
 
   const handleToggleComplete = (id: string) => {
     const newShoppingList = shoppingList.map((item) => {
       if (item.id === id) {
+        if (item.completedAtTimestamp) {
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+        } else {
+          Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+        }
         return {
           ...item,
           completedAtTimestamp: item.completedAtTimestamp
